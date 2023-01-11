aafeddd843d9805eea80091cfa080cfd6ef84b16
Timilehin Arigbede <harriemannie@gmail.com>
2023-01-11 20:02:46 +0100

    Connecting Widgets & Managing Data / State

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -3,12 +3,13 @@ import '../models/transaction.dart';
 import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
-  const TransactionList({super.key});
+  final List<Transaction> transactions;
+  TransactionList(this.transactions);
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: _userTransactions.map((tx) {
+      children: transactions.map((tx) {
         return Card(
           child: Row(
             children: [
